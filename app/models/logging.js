2ecf9a73eb93d0e0582747667d3e01d0fb34cf97
Dylan Kok <tenkotofu@gmail.com>
2021-09-06 21:10:33 +0800

    Give limits and logging models distinct IDs

--- app/models/logging.js
+++ app/models/logging.js
@@ -1,9 +1,18 @@
+// UUID & Hashing
+const uuid = require('uuid')
+
 const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
     class Log extends Model {}
 
     Log.init(
         {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: DataTypes.UUID,
+                defaultValue: uuid.v4(),
+            },
             type: {
                 allowNull: false,
                 type: DataTypes.STRING,
