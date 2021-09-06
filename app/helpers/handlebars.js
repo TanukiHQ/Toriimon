54f4e8c67db0105217b58ca90f0625c0cc8172fd
Dylan Kok <tenkotofu@gmail.com>
2021-09-06 21:09:35 +0800

    Added ifNotEquals helper

--- app/helpers/handlebars.js
+++ app/helpers/handlebars.js
@@ -8,6 +8,13 @@ module.exports = {
             return options.inverse(this)
         }
     },
+    ifNotEquals(a, b, options) {
+        if (a !== b) {
+            return options.fn(this)
+        } else {
+            return options.inverse(this)
+        }
+    },
     parseISODateTime: (value) => {
         return dateFormat(value, 'dS mmmm yyyy, HH:MM:ss')
     },
